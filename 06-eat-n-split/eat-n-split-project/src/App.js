5a9947dbe76976b0937fda0743d5e3420493ad7d
ali27-dev <khanali3229@gmail.com>
2025-12-08 22:06:09 +0500

    The First friendlist component is complelete.

--- 06-eat-n-split/eat-n-split-project/src/App.js
+++ 06-eat-n-split/eat-n-split-project/src/App.js
@@ -1,9 +1,74 @@
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.id} />
+      <h3>{friend.name}</h3>
+      <p>
+        {friend.balance < 0 && (
+          <p className="red">
+            You owe {friend.name} {Math.abs(friend.balance)}
+          </p>
+        )}
+      </p>
+      <p>
+        {friend.balance > 0 && (
+          <p className="green">
+            You owe {friend.name} {Math.abs(friend.balance)}
+          </p>
+        )}
+      </p>
+      <p>
+        {friend.balance === 0 && (
+          <p>
+            You owe {friend.name} {Math.abs(friend.balance)}
+          </p>
+        )}
+      </p>
+      <button className="button">Select</button>
+    </li>
+  );
+}
