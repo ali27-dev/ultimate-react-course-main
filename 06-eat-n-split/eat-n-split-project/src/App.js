eb981ec72bd8bdefaeb5091dd3ca2a462628ff54
ali27-dev <khanali3229@gmail.com>
2025-12-10 08:40:09 +0500

    Putting functionality in addFriend form its going correct but there no functionality for adding in friend list.

--- 06-eat-n-split/eat-n-split-project/src/App.js
+++ 06-eat-n-split/eat-n-split-project/src/App.js
@@ -55,7 +55,7 @@ function FriendList() {
   return (
     <ul>
       {friends.map((friend) => (
-        <Friend friend={friend} />
+        <Friend friend={friend} key={friend.id} />
       ))}
     </ul>
   );
@@ -86,16 +86,43 @@ function Friend({ friend }) {
 }
 
 function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48?u=499476");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID();
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance: 0,
+    };
+
+    console.log(newFriend);
+
+    setName("");
+    setImage("https://i.pravatar.cc/48?u=499476");
+  }
+
   return (
     <>
       <div className="form">
-        <form className="form-add-friend">
+        <form className="form-add-friend" onSubmit={handleSubmit}>
           <label htmlFor="">👬 Friend name</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <label htmlFor="">🌄 Image URL</label>
           <input
             type="text"
-            placeholder={"https://i.pravatar.cc/48?u=499476"}
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
           />
           <Button>Add</Button>
         </form>
